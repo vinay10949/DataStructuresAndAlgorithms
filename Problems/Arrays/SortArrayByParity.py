91211ee25d5ce1b75891371812cd761111257741
Vinay Sawant <vinay.sawant@mintwalk.com>
2020-07-12 12:56:06 +0530

    SortArrayByParity https://leetcode.com/problems/sort-array-by-parity/

---
+++ Problems/Arrays/SortArrayByParity.py
@@ -0,0 +1,45 @@
+'''
+Problem Statement:
+
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+Example 1:
+
+Input: [3,1,2,4]
+
+Output: [2,4,3,1]
+
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Note:
+
+1 <= A.length <= 5000
+
+0 <= A[i] <= 5000
+
+Time Complexity : O(n)
+Space Complexity: O(1)
+'''
+
+class Solution(object):
+    def sortArrayByParity(self,nums):
+        """
+        :type nums: List(int)
+        :rtype nums: List(int)
+        """
+        i=0
+        j=len(nums)-1
+
+        while i<j:
+            if nums[i]%2!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j-=1
+            else:
+                i+=1
+        return nums
+
+s=Solution()
+print(s.sortArrayByParity([3,1,2,4]))
+
