0291cd5a4e51a0d75d229894adfcaf274ce4e999
Vinay Sawant <vinay.sawant@mintwalk.com>
2020-07-12 13:08:25 +0530

    RunTime analysis for Selection Sort Added

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -9,3 +9,13 @@ def selection_sort(arr):
 arr = [4,6,7,9,0,-2,9.4]
 selection_sort(arr)
 print(arr)
+
+'''
+Class	Sorting algorithm
+Data structure	Array
+Worst-case performance	О(n^2) comparisons, О(n) swaps
+Best-case performance	О(n^2) comparisons, O(1) swaps
+Average performance	О(n2) comparisons, О(n) swaps
+Worst-case space complexity	O(1) auxiliary
+
+'''
