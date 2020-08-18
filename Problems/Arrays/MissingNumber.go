299c142f91fc40941ecfd52464ff3c5193b66709
vinay <vinay10949@gmail.com>
2020-08-18 17:48:33 +0530

    Find Missing Number from an array in Go, using O(1) Space and O(n) time complexity

---
+++ Problems/Arrays/MissingNumber.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "sort"
+)
+
+func main(){
+
+    arr:=[]int{1,3,4,5,6}
+    fmt.Println("Given Array ",arr)
+    fmt.Println("Find missing element in an array using O(1) Space and O(n) time complexity")
+    sort.Ints(arr)
+    maxEle:=arr[len(arr)-1]
+
+    x:=0
+    y:=0
+    for _,i :=range arr{
+        x=x^i
+    }
+
+    for i := 1; i <= maxEle; i++ {
+        y=y^i
+    }
+    fmt.Println(fmt.Sprintf("Missing Element in array is %d ",x^y))
+}
